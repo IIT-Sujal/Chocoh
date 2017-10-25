ba12146198079f4b614407719087c201a5145250
Sujal <sujal.maheswari.cse15@itbhu.ac.in>
2017-10-25 23:26:59 +0530

    Database Created Basic

--- home/admin.py
+++ home/admin.py
@@ -2,7 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import User
+from .models import User,Order,Cart,Feedback,Shipper,Payment,Bank_Detail,Chocolate
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Order)
+admin.site.register(Cart)
+admin.site.register(Feedback)
+admin.site.register(Shipper)
+admin.site.register(Payment)
+admin.site.register(Bank_Detail)
+admin.site.register(Chocolate)
+
